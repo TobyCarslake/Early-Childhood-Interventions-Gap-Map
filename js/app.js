b7bfefb9b502fbf2a7927309876bad1742e9f8b1
TobyCarslake <60861635+TobyCarslake@users.noreply.github.com>
2020-05-15 16:31:56 +0930

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// fetch(json\data.csv)
+// let data = .\data.csv
+
+// let incAfrica = [];
+
+
+// function 1(){
+//     if (data.)
+//     incAfrica 
+// }
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//        // Typical action to be performed when the document is ready:
+//        document.getElementById("demo").innerHTML = xhttp.responseText;
+//     }
+// };
+// xhttp.open("GET", "data.json", true);
+// xhttp.send();
+
+tippy('#income_africa', {
+    content: "Effective interventions to strengthen early language and literacy skills in low-income countries"
+  });
+
+const incomeAfrica = [];
