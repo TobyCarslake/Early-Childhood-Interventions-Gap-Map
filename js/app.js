1fc94377e0a7cf0131937b16bd85720c3a43e129
Carslake <Toby.Carslake@acer.org>
2020-05-29 17:09:54 +0930

    all rows setup up added shadow

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ xhttp.onreadystatechange = function() {
       //  document.getElementById("demo").innerHTML = xhttp.responseText;
       const response = JSON.parse(xhttp.responseText);
       console.log(response.data);
-      data = response.data;
+      var data = response.data;
       // const incAfr = Object.keys(data).filter(k=> data[k].Intervention == 'Income supplementation') && (k=> data[k].Intervention == 'Africa and the Middle East'.map(k=>data[k]).sort((a,b)=>+a.ID-b.ID));
       // console.log(incAfr);
       let incomeAfrica = '';
@@ -74,24 +74,17 @@ tippy('#income', {
   placement: 'auto-end',
   interactive: true
 });
-console.log(data);
 
-var data = { 5: { "ID": "0", "Status": "Valid" } }; // some OBJECT from server response
-
-data = { ...data,
-  0: { "ID": "1", "Status": "Valid" },
-  1: { "ID": "2", "Status": "Invalid" },
-  2: { "ID": "3", "Status": "Valid" }
-}
-
-
-// https://stackoverflow.com/questions/8925820/javascript-object-push-function
-// // solution 1: where output is sorted filtred array
-// let arr=Object.keys(data).filter(k=> data[k].Status!='Invalid').map(k=>data[k]).sort((a,b)=>+a.ID-b.ID);
-  
-// // solution2: where output is filtered object
-// Object.keys(data).map(k=> data[k].Status=='Invalid' && delete data[k])
-  
-// // show
-// console.log('Object',data);
-// console.log('Array ',arr);
+tippy('#parent', {
+  maxWidth: '',
+  trigger: 'click',
+  hideOnClick: 'false',
+  content: 'This category includes studies in which the focal point of the intervention is the parent, or wider family group.',
+  allowHTML: true,
+  appendTo: document.body,
+  boundary: 'window',
+  placement: 'auto',
+  placement: 'auto-start',
+  placement: 'auto-end',
+  interactive: true
+});
